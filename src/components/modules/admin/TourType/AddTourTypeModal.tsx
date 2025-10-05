72da31b5abb73b0c5101baadf6256c6cac55bfbe
AkibKST <akibkst22@gmail.com>
2025-10-05 09:10:13 +0600

    Managing Data Display with ShadCN Table

---
+++ src/components/modules/admin/TourType/AddTourTypeModal.tsx
@@ -0,0 +1,45 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export function AddTourTypeModal() {
+  return (
+    <Dialog>
+      <form>
+        <DialogTrigger asChild>
+          <Button>Add Tour Type</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Add Tour Type</DialogTitle>
+            <DialogDescription>
+              Add a new tour type here. Click save when you are done.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="grid gap-4">
+            <div className="grid gap-3">
+              <Label htmlFor="name-1">Name</Label>
+              <Input id="name-1" name="name" defaultValue="Pedro Duarte" />
+            </div>
+          </div>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant="outline">Cancel</Button>
+            </DialogClose>
+            <Button type="submit">Save changes</Button>
+          </DialogFooter>
+        </DialogContent>
+      </form>
+    </Dialog>
+  );
+}
